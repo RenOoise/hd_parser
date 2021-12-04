7ef281eb6d74e40e3ae0d602ce65b4b0ac8c8faa
Anton Shields <renOoise@gmail.com>
2021-12-05 01:27:47 +0200

    [progress] добавлены хендлеры /login /start, распознавание пароля и регистрация в боте

--- bot/management/commands/bot.py
+++ bot/management/commands/bot.py
@@ -9,7 +9,10 @@ from telegram.ext import MessageHandler
 from telegram.ext import Updater
 from telegram import Update
 from telegram.utils.request import Request
-
+from telegram import InlineKeyboardButton
+from telegram import InlineKeyboardMarkup
+from telegram.ext import CallbackQueryHandler
+from telegram.ext import Handler
 from bot.models import Profile
 
 
@@ -24,31 +27,76 @@ def log_errors(f):
     return inner
 
 
-"""Обработчик команды /start
+"""
+Обработчик команды /start
 """
 
 
 @log_errors
 def do_start(update: Update, context: CallbackContext):
     chat_id = update.message.chat_id
-
     if update.message.from_user.username is None:
         username = 'NoName'
     else:
         username = update.message.from_user.username
-    p, _ = Profile.objects.get_or_create(
-        external_id=chat_id,
-        defaults={
-            'name': username
-        }
-    )
-    update.message.reply_text("Я бот, который будет присылать тебе заявки из ХД, "
-                              "если ты обладаешь паролем для авторизации \n"
-                              "Для справки пишите /help"
-                              )
+
+    if Profile.objects.filter(external_id=chat_id).exists():
+        if Profile.objects.get().is_registered:
+            update.message.reply_text(f"Привет, {username}! Я бот, который будет присылать тебе заявки из ХД "
+                                      "по мере их поступления.\n"
+                                      "Сбор заявок с веб-интерфейса выполняется каждые 5 минут, так что могут "
+                                      "быть небольшие задержки"
+                                      "Для списка команд  пиши /help"
+                                      )
+        else:
+            update.message.reply_text(f'Повторяю еще раз: тебе, {username}, нужен пароль для входа.\n'
+                                      'Если он у тебя есть, то пиши /login')
+    else:
+        p, _ = Profile.objects.get_or_create(
+            external_id=chat_id,
+            defaults={
+                'name': username
+            }
+        )
+        update.message.reply_text(f'Привет, {username}!\n'
+                                  'Для получения доступа к этому боту нужно обладать паролем для регистрации.\n'
+                                  'Если он у тебя есть используй команду /login для входа.'
+                                  )
 
 
-"""Обработчик команды /help
+'''
+Обработчик команды /login
+'''
+
+
+@log_errors
+def do_login(update: Update, context: CallbackContext):
+    chat_id = update.message.chat_id
+    if update.message.from_user.username is None:
+        username = 'NoName'
+    else:
+        username = update.message.from_user.username
+
+    if Profile.objects.filter(external_id=chat_id).exists():
+        if Profile.objects.get().is_registered:
+            update.message.reply_text(f"{username}! Ты уже зарегистрирован. Если хочешь подписаться на уведомления "
+                                      "для какого-то определенного исполнителя, воспользуйся командой /subscribe\n"
+                                      "Для списка команд  пиши /help"
+                                      )
+        else:
+            update.message.reply_text('Вводи свой пароль после этого сообщения')
+    else:
+        p, _ = Profile.objects.get_or_create(
+            external_id=chat_id,
+            defaults={
+                'name': username
+            }
+        )
+        update.message.reply_text('Вводи свой пароль после этого сообщения')
+
+
+"""
+Обработчик команды /help
 """
 
 
@@ -67,6 +115,33 @@ def do_help(update: Update, context: CallbackContext):
                               "/logout - сбросить текущую авторизацию \n"
                               "/stop - не присылать уведомления \n"
                               )
+
+
+@log_errors
+def do_register(update: Update, context: CallbackContext):
+    chat_id = update.message.chat_id
+    if update.message.from_user.username is None:
+        username = 'NoName'
+    else:
+        username = update.message.from_user.username
+
+    if Profile.objects.filter(external_id=chat_id).exists():
+        print('В базе есть')
+        if Profile.objects.get().is_registered:
+            print('Зареган')
+            update.message.reply_text('Команда не распознана.\n'
+                                      'Если хочешь пообщаться, то это не ко мне.')
+        else:
+            print(update.message.text)
+            print(settings.SUBSCRIPTION_KEY)
+            if update.message.text == settings.SUBSCRIPTION_KEY:
+                profile_register = Profile.objects.get(external_id=chat_id)
+                profile_register.is_registered = True
+                profile_register.save()
+                update.message.reply_text('Странно, ты откуда-то знаешь пароль.\n'
+                                          'Добро пожаловать!\n'
+                                          'Теперь можешь подписаться на нужные тебе заявки.\n'
+                                          'Просто напиши /subscribe и выбери исполнителя.')
 
 
 class Command(BaseCommand):
@@ -98,6 +173,12 @@ class Command(BaseCommand):
         help_handler = CommandHandler('help', do_help)
         updater.dispatcher.add_handler(help_handler)
 
+        login_handler = CommandHandler('login', do_login)
+        updater.dispatcher.add_handler(login_handler)
+
+        # Обработчик всех сообщений
+        message_handler = MessageHandler(Filters.text, do_register)
+        updater.dispatcher.add_handler(message_handler)
 
         # Запустить бесконечную обработку входящих сообщений
         updater.start_polling()
